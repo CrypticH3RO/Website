0734f1ffc00c352909d577fbdb0b6201cbf2ae92
CrypticH3RO <aamir.haroon@mail.utoronto.ca>
2018-08-08 16:09:05 -0400

    Added working screen on the phone

--- phone.js
+++ phone.js
@@ -1,10 +1,33 @@
+/*
 test.onclick = function(){
 	screen = document.querySelector("number");
 	screen.innerHTML = this.innerHTML;
-}
-function myFunction() {
-	screen = document.querySelector("number");
-	value = document.querySelector("test")
-	screen.innerHTML = value.innerHTML;
 	alert("Hi");
 }
+*/
+function myFunction2() {
+	/*
+	screen = document.querySelector("number");
+	value = document.querySelector("test");
+	screen.innerHTML = value.innerHTML;
+	*/
+	alert("Hi");
+}
+function start() {
+// Get all the span elements in the key
+var keys = document.getElementById("keys1");
+var keyA = keys.getElementsByTagName("span");
+/* Could also do this
+var keys = document.querySelectorAll('#keys1 span');
+*/
+// Get the screen
+screen = document.getElementById("screen1");
+//Set up an onclick event for each span key
+for (i=0; i < keyA.length; i++){
+	keyA[i].onclick = function(){
+		screen.innerHTML += this.innerHTML;
+	}
+}
+}
+
+
